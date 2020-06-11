b152410d4a7a1f57fdd9c8be7de8de5cd9f3da73
bizui18 <66097247+bizui18@users.noreply.github.com>
2020-06-11 15:40:28 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1846,7 +1846,7 @@ while True:
 						aa.append(bossTime[i])                           #output_bossData[1] : 시간 
 						aa.append(bossTime[i].strftime('%H:%M:%S'))      #output_bossData[2] : 시간(00:00:00) -> 초빼기 : aa.append(bossTime[i].strftime('%H:%M'))  
 						
-						m_code = ['탈킨','발보','셀루','스탄',/
+						m_code = ['탈킨','발보','셀루','스탄',\
 							  '릴리']
 						chk = 0
 						for m_name in m_code:
