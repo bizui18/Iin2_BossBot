fb04893e481ffacd1d94f8f29f00bc6aa888ad40
bizui18 <66097247+bizui18@users.noreply.github.com>
2020-06-11 13:22:34 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1846,7 +1846,7 @@ while True:
 						aa.append(bossTime[i])                           #output_bossData[1] : 시간 
 						aa.append(bossTime[i].strftime('%H:%M:%S'))      #output_bossData[2] : 시간(00:00:00) -> 초빼기 : aa.append(bossTime[i].strftime('%H:%M'))  
 						if bossData[i][0] == '바실라' :
-							aa.append('-')				 #output_bossData[3] : +
+							aa.append('-')				 #output_bossData[3] : -
 						else :
 							aa.append('+')				 #output_bossData[3] : +
 					aa.append(bossData[i][2])                            #output_bossData[4] : 멍/미입력 보스
@@ -2008,7 +2008,10 @@ while True:
 					else :
 						aa.append(bossTime[i])                           #output_bossData[1] : 시간
 						aa.append(bossTime[i].strftime('%H:%M:%S'))      #output_bossData[2] : 시간(00:00:00) -> 초빼기 : aa.append(bossTime[i].strftime('%H:%M'))
-						aa.append('+')	                                 #output_bossData[3] : +
+						if bossData[i][0] == '바실라' :
+							aa.append('-')				 #output_bossData[3] : -
+						else :
+							aa.append('+')				 #output_bossData[3] : +
 					aa.append(bossData[i][2])                            #output_bossData[4] : 멍/미입력 보스
 					aa.append(bossMungCnt[i])	                         #output_bossData[5] : 멍/미입력횟수
 					aa.append(bossData[i][6])	                         #output_bossData[6] : 메세지
